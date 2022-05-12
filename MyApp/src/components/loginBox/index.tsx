67a43ce1477303f9ac0d3c5b0923439f712f3a68
Lucas Almeida <lucas.almeida@qesh.ai>
2022-05-12 16:13:53 -0300

    animação na tela de login implementada. :sparkles:

--- MyApp/src/components/loginBox/index.tsx
+++ MyApp/src/components/loginBox/index.tsx
@@ -1,7 +1,9 @@
 /* eslint-disable react-native/no-inline-styles */
-import React, {useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {
   Alert,
+  Animated,
+  Dimensions,
   Keyboard,
   StyleSheet,
   Text,
@@ -22,6 +24,32 @@ export default function LoginBox() {
     ToastAndroid.showWithGravity(msg, ToastAndroid.LONG, ToastAndroid.BOTTOM);
   };
 
+  const moveAnim = useRef(new Animated.Value(0)).current;
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.sequence([
+      Animated.timing(moveAnim, {
+        duration: 2000,
+        toValue: Dimensions.get('window').width / 1.8,
+        delay: 0,
+        useNativeDriver: false,
+      }),
+      Animated.timing(moveAnim, {
+        duration: 2000,
+        toValue: 0,
+        delay: 0,
+        useNativeDriver: false,
+      }),
+    ]).start();
+    Animated.timing(fadeAnim, {
+      duration: 2000,
+      toValue: 1,
+      delay: 2400,
+      useNativeDriver: false,
+    }).start();
+  }, [moveAnim, fadeAnim]);
+
   function Logado() {
     if (user === '' && password === '') {
       Alert.alert('tudo ok');
@@ -39,7 +67,7 @@ export default function LoginBox() {
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <View style={styles.boxView}>
+      <Animated.View style={[styles.boxView, {opacity: fadeAnim}]}>
         <Text />
         <InputContainer style={{borderWidth: 2.2, borderColor: color}}>
           <View style={styles.IconView}>
@@ -106,7 +134,7 @@ export default function LoginBox() {
             <Text style={styles.FooterText}>registrar</Text>
           </TouchableOpacity>
         </Footer>
-      </View>
+      </Animated.View>
     </TouchableWithoutFeedback>
   );
 }
--- MyApp/src/pages/loginPage/login.tsx
+++ MyApp/src/pages/loginPage/login.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import {
   Text,
   View,
@@ -7,23 +7,65 @@ import {
   StyleSheet,
   TouchableWithoutFeedback,
   Keyboard,
+  Animated,
+  Dimensions,
 } from 'react-native';
 import {Container} from './styles';
 import LoginBox from '../../components/loginBox';
 
 export default function App() {
+  const moveAnim = useRef(new Animated.Value(0)).current;
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.sequence([
+      Animated.timing(moveAnim, {
+        duration: 2000,
+        toValue: Dimensions.get('window').width / 1.8,
+        delay: 0,
+        useNativeDriver: false,
+      }),
+      Animated.timing(moveAnim, {
+        duration: 2000,
+        toValue: 0,
+        delay: 0,
+        useNativeDriver: false,
+      }),
+    ]).start();
+    Animated.timing(fadeAnim, {
+      duration: 2000,
+      toValue: 1,
+      delay: 2400,
+      useNativeDriver: false,
+    }).start();
+  }, [moveAnim, fadeAnim]);
+
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <Container>
         <StatusBar backgroundColor={'#1e4d5cf6'} />
-        <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+        <Animated.View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'center',
+            marginLeft: moveAnim,
+          }}>
           <Text style={styles.text}>M</Text>
-          <Text style={styles.text}>OVIES</Text>
-        </View>
-        <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+          <Animated.Text style={[styles.text, {opacity: fadeAnim}]}>
+            OVIES
+          </Animated.Text>
+        </Animated.View>
+        <Animated.View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'center',
+            marginLeft: moveAnim,
+          }}>
           <Text style={styles.text}>A</Text>
-          <Text style={styles.text}>PP</Text>
-        </View>
+          <Animated.Text style={[styles.text, {opacity: fadeAnim}]}>
+            PP
+          </Animated.Text>
+        </Animated.View>
         <LoginBox />
         <View
           style={{
